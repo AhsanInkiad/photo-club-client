c19af5c1ef8ab6b502ff2f3356b92a2f1b600047
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2023-06-10 18:29:24 +0600

    Delete is working in Selected Class component

--- src/pages/classes/Classes.jsx
+++ src/pages/classes/Classes.jsx
@@ -2,66 +2,98 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Classes.css'
 import { AuthContext } from '../../providers/AuthProvider';
+import Swal from 'sweetalert2';
 
 const Classes = () => {
     useEffect(() => {
         document.title = "PhotoHero | Classes";
     }, []);
-    const { setCount, count } = useContext(AuthContext);
+
+    const { setCount, count, user } = useContext(AuthContext);
     const [tabs, setTabs] = useState([]);
+
     useEffect(() => {
         fetch('http://localhost:5000/classes')
             .then(res => res.json())
             .then(data => setTabs(data))
-    }, [])
+    }, []);
 
     console.log(tabs);
 
+    const handleSelect = (tab) => {
+        const selectedClass = {
+            userName: user.displayName,
+            userEmail: user.email,
+            name: tab.name,
+            image: tab.image,
+            instructor_name: tab.instructor_name,
+            instructor_email: tab.instructor_email,
+            price: tab.price,
+        }
+
+        console.log(selectedClass);
+        fetch('http://localhost:5000/selectedclass', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(selectedClass)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire(
+                        'Done!',
+                        'Class selected.',
+                        'success'
+                    );
+                }
+            });
+    };
+
     return (
         <div className='bg-white'>
-
-            { /* Our Classes */}
+            {/* Our Classes */}
             <div className='pt-40 pb-40'>
                 <p className='pb-10 text-black text-center text-2xl font-semibold lg:text-4xl lg:font-bold'>Our Classes</p>
                 {/* Popular classes card */}
-                <div className='mx-auto grid grid-cols-1 w-3/4  justify-items-center lg:grid-cols-3 gap-10 px-10 lg:px-28'>
+                <div className='mx-auto grid grid-cols-1 w-3/4 justify-items-center lg:grid-cols-3 gap-10 px-10 lg:px-28'>
                     {/* Card-1 */}
-
-                    {
-                        tabs.map((tab) => (
-                            <div>
-                                <div className="card bg-white border-2 border-[#1E293B] shadow-xl hover:scale-110 transition duration-300">
-                                    <div>
-                                        <img className="daimg pt-10  mx-auto rounded-xl  hover:scale-110 transition duration-300" src={tab.image} />
+                    {tabs.map((tab) => (
+                        <div key={tab.id}>
+                            <div className="card bg-white border-2 border-[#1E293B] shadow-xl hover:scale-110 transition duration-300">
+                                <div>
+                                    <img className="daimg pt-10 mx-auto rounded-xl hover:scale-110 transition duration-300" src={tab.image} />
+                                </div>
+                                <div className="card-body text-black">
+                                    <h2 className="card-title mx-auto font-bold text-2xl text-center">{tab.name} (${tab.price})</h2>
+                                    <p className="text-center text-slate-600 font-semibold">Instructor: <br />{tab.instructor_name}</p>
+                                    <div className='pt-1 lg:pt-5 text-center'>
+                                        <p className="text-[#1DA1F2] text-2xl lg:text-3xl font-bold">{tab.available_seats}</p>
+                                        <p className="text-center text-slate-600 font-semibold text-sm">Seats Available</p>
                                     </div>
-
-                                    <div className="card-body text-black">
-                                        <h2 className="card-title mx-auto font-bold text-2xl text-center">{tab.name} (${tab.price})</h2>
-                                        <p className="text-center text-slate-600 font-semibold">Instructor: <br></br>{tab.instructor_name}</p>
-                                        <div className='pt-1 lg:pt-5 text-center '>
-                                            <p className="text-[#1DA1F2] text-2xl lg:text-3xl font-bold">{tab.available_seats}</p>
-                                            <p className="text-center text-slate-600 font-semibold text-sm">Seats Available</p>
-                                        </div>
-                                        <div className="card-actions justify-center">
-                                            {count ? (
-                                                <></>
-                                            ) : (
-                                                <Link to="/dashboard/selectedclasses">
-                                                    <button className="mt-1 lg:mt-5 btn btn-outline btn-info btn-xs sm:btn-sm md:btn-md lg:btn-lg font-bold">
+                                    <div className="card-actions justify-center">
+                                        {count ? (
+                                            <></>
+                                        ) : (
+                                            tab.status === 'approved' ? (
+                                                
+                                                    <button onClick={() => handleSelect(tab)} className="mt-1 lg:mt-5 btn btn-outline btn-info btn-xs sm:btn-sm md:btn-md lg:btn-lg font-bold">
                                                         Select
                                                     </button>
-                                                </Link>
-                                            )}
-                                        </div>
+                                                
+                                            ) : (tab.status === 'pending' ? (
+                                                <p className="mt-1 lg:mt-5 text-center text-yellow-500 font-semibold">Approval {tab.status}! </p>
+                                            ) : (
+                                                <p className="mt-1 lg:mt-5 text-center text-red-500 font-semibold">Approval of this class is denied!</p>
+                                            ))
+                                        )}
                                     </div>
                                 </div>
                             </div>
-
-                        ))
-
-                    }
-
-
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
--- src/pages/dashboard/selected classes/SelectedClasses.jsx
+++ src/pages/dashboard/selected classes/SelectedClasses.jsx
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './selectedclasses.css'
+import { AuthContext } from '../../../providers/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
+import Swal from 'sweetalert2';
 
 const SelectedClasses = () => {
+    const { user } = useContext((AuthContext));
+    const [data, setData] = useState([]);
+    const [hoveredRow, setHoveredRow] = useState(null);
+    const url = `http://localhost:5000/sClasses?userEmail=${user.email}`;
+
+    // tanstack query
+    const { data: sClasses = [], refetch } = useQuery(['sClasses'], async () => {
+        const res = await fetch(url)
+        return res.json();
+    })
+    console.log('tanstack er data niche:')
+    console.log(sClasses);
+    const handleRowHover = (index) => {
+        setHoveredRow(index);
+    };
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/del/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(datas => {
+                        console.log(datas);
+                        if (datas.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                            refetch();
+                        }
+                       
+                    })
+              
+            }
+        })
+}
+
+
     return (
         <div>
 
@@ -25,40 +77,48 @@ const SelectedClasses = () => {
                         <tbody>
                             {/* row 1 */}
 
-                            <tr>
+                            {
+                                sClasses.map((tab, index) => (
+                                    <tr
+                                        key={tab.id}
+                                        className={hoveredRow === index ? 'hovered-row' : ''}
+                                        onMouseEnter={() => handleRowHover(index)}
+                                        onMouseLeave={() => handleRowHover(null)}>
 
-                                <td className=''>
-                                    <div className="flex items-center space-x-3">
-                                        <div className="avatar">
-                                            <div className="mask mask-squircle w-32 h-32">
-                                                <img src="https://images.unsplash.com/photo-1580428261891-cb29e9e409e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" />
+                                        <td className=''>
+                                            <div className="flex items-center space-x-3">
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle w-32 h-32">
+                                                        <img className="zoom-image" src={tab.image} />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="text-black font-semibold">{tab.name}</div>
+
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div>
-                                            <div className="text-black font-semibold">asdasd</div>
+                                        </td>
+                                        <td className='text-black text-center font-semibold'>
+                                            {tab.instructor_name}
+                                        </td>
+                                        <td className='text-black text-center font-semibold'>{tab.instructor_email}</td>
+                                        <td className='text-black text-center font-semibold'>${tab.price}</td>
 
-                                        </div>
-                                    </div>
-                                </td>
-                                <td className='text-black text-center font-semibold'>
-                                    dddd
-                                </td>
-                                <td className='text-black text-center font-semibold'>qqwssqd</td>
-                                <td className='text-black text-center font-semibold'>dfgdfg</td>
+                                        <th>
+                                            <button onClick={() => handleDelete(tab._id)} className=" Nabutton Nanoselect"><span className="Natext">Delete</span><span className="Naicon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
+                                        </th>
+                                        <th>
+                                            <Link to='/dashboard/payment'> <button className="btn btn-outline btn-info">Make Payment</button></Link>
 
-                                <th>
-                                    <button className=" Nabutton Nanoselect"><span className="Natext">Delete</span><span className="Naicon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
-                                </th>
-                                <th>
-                                    <Link to='/dashboard/payment'> <button className="btn btn-outline btn-info">Make Payment</button></Link>
+                                        </th>
+                                    </tr>
+                                ))
+                            }
 
-                                </th>
-                            </tr>
 
-                           
 
                         </tbody>
-                        
+
                     </table>
                 </div>
             </div>
