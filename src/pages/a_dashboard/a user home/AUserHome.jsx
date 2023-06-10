1a1aeb43c3644c1ff1467a5dab7ed67c61da5600
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2023-06-10 23:06:05 +0600

    All the bugs in every dashboard is fixed now

--- src/pages/a_dashboard/a user home/AUserHome.jsx
+++ src/pages/a_dashboard/a user home/AUserHome.jsx
@@ -3,7 +3,32 @@ import './AUserHome.css';
 const AUserHome = () => {
     return (
         <div>
-            <p>Admin user home</p>
+       
+            <div className='flex gap-60 items-center border-2 rounded-lg border-[#1E293B] p-24'>
+                <div>
+                    <div className=' rounded-full '>
+                        <img className='dp  ' src="" alt="" />
+
+                    </div>
+                    <p className='text-[#1E293B] pt-4 font-bold text-lg'>Name: </p>
+
+                </div>
+                <div className="wcards">
+                    <div className="wcard wred">
+                        <p className="wtip text-lg">Total Users: </p>
+                        <p className="wsecond-text text-2xl font-bold"></p>
+                    </div>
+                    <div className="wcard wred">
+                        <p className="wtip text-lg">Total Classes:</p>
+                        <p className="wsecond-text text-2xl font-bold"></p>
+                    </div>
+                    <div className="wcard wblue">
+                        <p className="wtip text-lg">Total Approved Classes:</p>
+                        <p className="wsecond-text text-2xl font-bold"></p>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/dashboard/user home/UserHome.jsx
+++ src/pages/dashboard/user home/UserHome.jsx
@@ -4,14 +4,22 @@ import { AuthContext } from '../../../providers/AuthProvider';
 import { useQuery } from '@tanstack/react-query';
 
 const UserHome = () => {
-    const { user, totalClass, setTotalClass,  setTotalSelectedClass, totalSelectedClass } = useContext(AuthContext);
+    const { user, totalClass, setTotalClass, setTotalSelectedClass, totalSelectedClass } = useContext(AuthContext);
     const [tabs, setTabs] = useState([]);
+    const [s, setS] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:5000/classes')
             .then(res => res.json())
             .then(data => setTabs(data))
     }, []);
+
+    const { data: sUsers = [], refetc } = useQuery(['users'], async () => {
+        const res = await fetch(`http://localhost:5000/sUsers?userEmail=${user.email}`)
+        return res.json();
+    })
+
+    console.log(sUsers);
 
     const url = `http://localhost:5000/sClasses?userEmail=${user.email}`;
 
@@ -22,26 +30,43 @@ const UserHome = () => {
     })
     setTotalSelectedClass(sClasses.length)
     setTotalClass(tabs.length);
-   
-    return (
-        <div>
-            <div className="wcards">
-                <div className="wcard wred">
-                    <p className="wtip text-lg">Total Available Classes: </p>
-                    <p className="wsecond-text text-2xl font-bold">{totalClass}</p>
-                </div>
-                <div className="wcard wred">
-                    <p className="wtip text-lg">Total Selected Classes:</p>
-                    <p className="wsecond-text text-2xl font-bold">{totalSelectedClass}</p>
-                </div>
-                <div className="wcard wblue">
-                    <p className="wtip text-lg">Total Enrolled Classes:</p>
-                    <p className="wsecond-text text-2xl font-bold">10</p>
-                </div>
 
-            </div>
-        </div>
+    return (
+        <>
+            {
+                sUsers.map(tab => (
+                    <div className='flex gap-60 items-center border-2 rounded-lg border-[#1E293B] p-24'>
+                        <div>
+                            <div className=' rounded-full '>
+                                <img className='dp  ' src={tab.photo} alt="" />
+
+                            </div>
+                            <p className='text-[#1E293B] pt-4 font-bold text-lg'>Name: {tab.name}</p>
+    
+                        </div>
+                        <div className="wcards">
+                            <div className="wcard wred">
+                                <p className="wtip text-lg">Total Available Classes: </p>
+                                <p className="wsecond-text text-2xl font-bold">{totalClass}</p>
+                            </div>
+                            <div className="wcard wred">
+                                <p className="wtip text-lg">Total Selected Classes:</p>
+                                <p className="wsecond-text text-2xl font-bold">{totalSelectedClass}</p>
+                            </div>
+                            <div className="wcard wblue">
+                                <p className="wtip text-lg">Total Enrolled Classes:</p>
+                                <p className="wsecond-text text-2xl font-bold">10</p>
+                            </div>
+
+                        </div>
+                    </div>
+                ))
+            }
+        </>
+
+
     );
 };
 
 export default UserHome;
+
